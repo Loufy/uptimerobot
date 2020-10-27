8164ce086d30fb91dcb218a32fedb495d555c0fb
Loufy <72176333+Loufy@users.noreply.github.com>
2020-10-27 11:57:09 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const axios = require("axios")
-const urls = ["https://takagi.glitch.me"]
+const urls = ["https://munari.glitch.me"]
 setInterval(function() {
             urls.forEach(url => {
             axios.get(url).then(console.log("Pong at " + Date.now())).catch(() => {});
